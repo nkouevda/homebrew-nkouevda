fbcfd59dceda99dc70c2c89bf5827ed0980d6dec
Nikita Kouevda <nkouevda@gmail.com>
2020-12-23 20:32:04 -0800

    capital-gains 1.0.5 (new formula)

---
+++ Formula/capital-gains.rb
@@ -0,0 +1,15 @@
+class CapitalGains < Formula
+  include Language::Python::Virtualenv
+
+  desc "Capital gains calculator"
+  homepage "https://pypi.org/project/capital-gains/"
+  url "https://github.com/nkouevda/capital-gains/archive/v1.0.5.tar.gz"
+  sha256 "d9d57aaa24ab7e1ecd3930b75f7f2d3269794f5eee50c3693b157fca1743ab2d"
+  license "MIT"
+
+  depends_on "python"
+
+  def install
+    virtualenv_install_with_resources
+  end
+end
