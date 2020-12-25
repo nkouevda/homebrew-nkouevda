956e3ec2886600114c428eb02a485e94792a7005
Nikita Kouevda <nkouevda@gmail.com>
2020-12-25 13:37:54 -0800

    capital-gains 1.0.6

--- Formula/capital-gains.rb
+++ Formula/capital-gains.rb
@@ -3,11 +3,16 @@ class CapitalGains < Formula
 
   desc "Capital gains calculator"
   homepage "https://github.com/nkouevda/capital-gains"
-  url "https://github.com/nkouevda/capital-gains/archive/v1.0.5.tar.gz"
-  sha256 "d9d57aaa24ab7e1ecd3930b75f7f2d3269794f5eee50c3693b157fca1743ab2d"
+  url "https://github.com/nkouevda/capital-gains/archive/v1.0.6.tar.gz"
+  sha256 "8429677ee880bcc2894c95dd86c107785860eef022d07523b5d82fe5286af673"
   license "MIT"
 
   depends_on "python"
+
+  resource "argparse-extensions" do
+    url "https://files.pythonhosted.org/packages/c7/f5/3f2960d89b6d79486b1777246865475f5c6abaa12fea16c152085b966f8c/argparse-extensions-1.0.3.tar.gz"
+    sha256 "9297ebcb86133c7e359cb7639dbba80a58712797b1fdab974c75053f26b993a7"
+  end
 
   def install
     virtualenv_install_with_resources
