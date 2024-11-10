7d5440465091e319da2da4e1803f31201c9a0a95
Nikita Kouevda <nkouevda@gmail.com>
2024-11-10 10:43:08 -0800

    estimated-taxes 2024.0

--- Formula/estimated-taxes.rb
+++ Formula/estimated-taxes.rb
@@ -3,15 +3,15 @@ class EstimatedTaxes < Formula
 
   desc "Estimated taxes calculator"
   homepage "https://github.com/nkouevda/estimated-taxes"
-  url "https://github.com/nkouevda/estimated-taxes/archive/v1.0.18.tar.gz"
-  sha256 "8944377b990359afbb9d058c584f9d849a572cd275c202d1155ecfb65568aa9c"
+  url "https://github.com/nkouevda/estimated-taxes/archive/v2024.0.tar.gz"
+  sha256 "2dcab58065f23cd4b64650a9e497a4cb06535b0e5c5aefacd244a03773ee3bcc"
   license "MIT"
 
   depends_on "python"
 
   resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/cd/e5/af35f7ea75cf72f2cd079c95ee16797de7cd71f29ea7c68ae5ce7be1eda0/PyYAML-6.0.1.tar.gz"
-    sha256 "bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
+    url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/pyyaml-6.0.2.tar.gz"
+    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
   end
 
   def install
