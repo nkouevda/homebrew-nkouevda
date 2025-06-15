80b0f722a60b9a9dd6a2141ea9b8759d10c219a3
Nikita Kouevda <nkouevda@gmail.com>
2025-06-14 23:12:51 -0700

    spongecase 1.0.3

--- Formula/spongecase.rb
+++ Formula/spongecase.rb
@@ -3,8 +3,8 @@ class Spongecase < Formula
 
   desc "Convert text to Spongebob case"
   homepage "https://github.com/nkouevda/spongecase"
-  url "https://github.com/nkouevda/spongecase/archive/v1.0.2.tar.gz"
-  sha256 "d9ee3345264c65bb90f4f6bb759b418044a8cf65e514ededa9d7e4bf86bad130"
+  url "https://github.com/nkouevda/spongecase/archive/v1.0.3.tar.gz"
+  sha256 "7dc4ab48282ace64b0e32d793ad4642008d9ddcff416dadfcd07189a56ffb8ed"
   license "MIT"
 
   depends_on "python"
